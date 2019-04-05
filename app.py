b8a9edace20a894c70d3434faef905a0dbb2d1b9
Sushen Biswas <sushen.biswas.aga@gmail.com>
2019-04-05 14:55:13 +0600

    Requirements and procfile

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 import sys
 from flask import Flask, request
 from pprint import pprint
+from pymessenger import Bot
+
+FB_ACCESS_TOKEN = "EAAfk8UkWEJgBAD2ATwNzKnoXg7B7ox0r9Rx1WQTxxQF0eoqfM1HcRn8KFQd9WzxDXMZCMZBHq2z2ksd1kuJNa4TTAPHLQyB02e0nEf8pgjKEeHd9YVavEK8LNhHPYBKSBJNHOiwqWa8sLGmx4jF36xlr6amjKentjWlybGw3oeRmxhyYIvihhk2kO4HZAgZD"
+
+bot = Bot(FB_ACCESS_TOKEN)
+
 
 app = Flask(__name__)
 
@@ -19,6 +25,27 @@ def verify():
 def webhook():
     data = request.get_json()
     log(data)
+    if data['object'] == 'page':
+        for entry in data['entry']:
+            for messaging_event in entry['messaging']:
+
+                # IDs
+                sender_id = messaging_event['sender']['id']
+                recipient_id = messaging_event['recipient']['id']
+
+                if messaging_event.get('message'):
+                    # Extracting text message
+                    if 'text' in messaging_event['message']:
+                        messaging_text = messaging_event['message']['text']
+                    else:
+                        messaging_text = 'no text'
+
+                # Echo Bot
+                response = messaging_text
+                bot.send_text_message(sender_id, response)
+
+
+
 
     return "ok", 200
 
